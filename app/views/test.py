462a3615ef26c34041bf2ab4e7535c19bcbf9ce6
swim135792468o <swim135792468o@taipay.com>
2020-08-10 01:19:36 +0000

    feat:新增入口網站部分的API 1.網站介紹有上傳和顯示 2.最新資訊有上傳、編輯、刪除和標題、內文、圖片個別的顯示

--- app/views/test.py
+++ app/views/test.py
@@ -10,6 +10,9 @@ from ..models import db
 from email.mime.text import MIMEText
 test = Blueprint('test', __name__)
 
+#設定時間格式
+ISOTIMEFORMAT = '%Y-%m-%d %H:%M:%S'
+
 @test.route('/USER/detect_repeated', methods=['POST'])
 def user_detect_repeated():
     if request.method == 'POST':
@@ -253,3 +256,233 @@ def reset_password_page(token):
         return render_template('test/reset_password.html')
     else:
         return "該網頁已過期"
+
+
+######################################################
+
+#入口網站資訊
+#網站介紹上傳
+#用json傳intro
+#回傳rspCode
+@test.route('/upload_web_intro', methods = ['POST'])
+def upload_web_intro(): 
+    if request.method == 'POST':    
+       #寫在webIntro.txt(路徑未定)
+       file = open(os.getcwd() + '\\' + 'webIntro.txt' , 'w')
+       #目前預設傳來的資訊叫intro
+       #且直接是str
+       json = request.get_json()
+       intro = json['intro']  
+       if file.write(intro):
+           file.close()
+           return jsonify({"rspCode": "200"})
+       else:       
+           file.close()       
+           #rspCode 400 檔案寫入失敗
+           return jsonify({"rspCode" : "400"})
+    else:
+        return jsonify({"rspCode":"300"})
+ 
+#網站介紹顯示
+#回傳rspCode,webIntro
+@test.route("/output_webIntro", methods = ['GET'])
+def output_web_intro():
+    if request.method == 'GET':
+        #開啟webIntro.txt(路徑未定)
+        try:
+            file = open(os.getcwd() + '\\' + "webIntro.txt", 'r')
+        except:
+            #rspCode 400:webIntro.txt開啟失敗
+            return jsonify({"rspCode" : "400","webIntro":''})
+        intro = jsonify({"rspCode":"200","webIntro":file.read()})
+        file.close()
+        return intro
+    else:
+        return jsonify({"repCode":"300","webIntro":''})    
+
+#最新資訊資訊上傳   
+#用form傳title,content,file(jpg,jpeg,png)
+@test.route('/upload_news', methods = ['POST'])
+def upload_news():
+    if request.method == 'POST':
+        #目前預設傳來的資訊叫content,title,file
+        #且文字直接是str
+        title = request.values['title']
+        content = request.values['content']
+        fileImage = request.files['file']
+        #檢查values是否為空
+        if title == '' or content == '' or fileImage.filename == '':
+            #rspCode 400:標題,內文,圖片有空值
+            return jsonify({"rspCode" : "400"})
+        #檢查tittle有沒有太大
+        if len(title) > 30:
+            #rspCode 405:title太長
+            return ({"rspCode":"405"})
+        time = str(datetime.datetime.now()).rsplit('.',1)[0]
+        try:
+            db.engine.execute(insert_news(title,time))
+        except:
+            #rspCode 404:標題上傳錯誤
+            return jsonify({"rspCode":"404"})
+        #目前預設傳來的圖片叫做file,允許jpg,gpeg,png(路徑未定)
+        allowExtention = set(['jpg', 'JPG', 'jpeg', 'JPEG', 'png', 'PNG'])
+        #檢查fileImage
+        if fileImage and '.' in fileImage.filename and fileImage.filename.rsplit('.', 1)[1] in allowExtention:
+            try:
+                #設定filename = newsID.jpg
+                filename = str(db.engine.execute(max_newsID()).fetchone()[0]) + '.jpg'
+                #儲存檔案到指定位置(未定)
+           #     os.makedirs(app.config['UPLOAD_FOLDER']
+           #     +'\\uploadFile\\newsImage')
+                fileImage.save(os.path.join(app.config['UPLOAD_FOLDER'] + '\\uploadFile\\newsImage' , filename))
+            except:
+                #rspCode 402:圖片上傳錯誤
+                return jsonify({"rspCode":"402"})
+        else:   
+            #rspCode 401:圖片檔名錯誤
+            return jsonify({"rspCode" : "401"})
+        try:    
+            #內文上傳
+            #(路徑未定)
+            fileContentName = str(db.engine.execute(max_newsID()).fetchone()[0]) + '.txt' 
+            fileContent = open(os.getcwd() + '\\newsContent\\' + fileContentName, 'w')
+            fileContent.write(content)
+            fileContent.close()
+            return jsonify({"rspCode":"200"})
+        except:
+            file.close()
+            #rspCode 403:內文上傳錯誤
+            return jsonify({"rspCode":"403"})
+    else:
+        return jsonify({"repCode":"300"})
+
+#最新資訊圖片顯示(路徑未定)
+#在網址帶入要顯示的編號
+#回傳rspCode,img
+@test.route("/output_news_image/<number>", methods = ['GET'])
+def output_newsImage(number):
+    if request.method == 'GET':
+        try:
+            if os.path.isfile(app.config["UPLOAD_FOLDER"] + '\\uploadFile\\newsImage' + "\\{}.jpg".format(number)):
+                filename = number + '.jpg'
+                return jsonify({"rspCode": "200", "img": filename})
+            else:
+                #這個編號沒檔案
+                return jsonify({"rspCode":"401","img":""})
+        except:
+            #圖片檔名獲取錯誤
+            return jsonify({"rspCode" : "400","img":""})
+    else:
+        return jsonify({"rspCode":"300","img":""})
+
+#最新資訊內文顯示(路徑未定)
+#在網址帶入要顯示的編號
+#回傳rspCode,content
+@test.route("/output_news_content/<number>", methods = ['GET'])
+def output_news_content(number):
+    if request.method == 'GET':
+
+        try:
+            filename = number + '.txt'
+            file = open(os.getcwd() + '\\newsContent\\' + filename, 'r')
+            content = jsonify({"rspCode": "200","content":file.read()})
+            file.close()
+            return content
+        except:
+            #內文顯示錯誤
+            return jsonify({"rspCode" : "400","content":""})
+    else:
+        return jsonify({"rspCode":"300","content":""})
+#最新消息標題顯示
+#在網址帶入要顯示的編號
+#回傳rspCode,title
+@test.route("/output_news_title/<number>", methods = ['GET'])
+def output_news_title(number):
+    if request.method == 'GET':
+        try:
+            return jsonify({"rspCode":"200","title":db.engine.execute(select_title(number)).fetchone()[0]})
+        except:
+            #最新消息顯示錯誤
+            return jsonify({"rspCode": "400","title":""})
+    else:
+        return jsonify({"rspCode":"300","title":""})
+#編輯最新消息
+#在網址帶入要編輯的編號
+#用form傳file(jpg,png,jpeg),title,content
+#回傳rspCode
+@test.route("/edit_news/<number>", methods = ['POST'])
+def edit_news(number):
+    if request.method == 'POST':
+        #目前預設傳來的圖片叫做file
+        fileImage = request.files['file']
+        title = request.values['title']
+        content = request.values['content']
+        if len(title) > 30:
+            #rspCode 404:title太長
+            return jsonify({"rspCode":"404"})
+        if fileImage.filename != '':
+            #允許jpg,gpeg,png(路徑未定)
+            allowExtention = set(['jpg', 'JPG', 'jpeg', 'JPEG', 'png', 'PNG'])
+            if fileImage and '.' in fileImage.filename and fileImage.filename.rsplit('.', 1)[1] in allowExtention:
+                try:
+                    #設定filename = newsID.jpg
+                    filename = number + '.jpg'
+                    #儲存檔案到指定位置(未定)
+                    os.remove(app.config['UPLOAD_FOLDER'] + '\\uploadFile\\newsImage' + "\\" + filename)
+                    fileImage.save(os.path.join(app.config['UPLOAD_FOLDER'] + '\\uploadFile\\newsImage', filename))
+                except:
+                    #rspCode 401:圖片更新失敗
+                    return jsonify({"rspCode":"401"})
+            else:
+                #rspCode 400:圖片檔名錯誤
+                return jsonify({"rspCode" : "400"})
+        if title != '':
+            try:    
+                db.engine.execute(update_title(title,number))
+            except:
+                #rspCode 402:標題更新失敗
+                return jsonify({"rspCode":"402"})
+        if content != '':
+            try:
+                file = open(os.getcwd() + "\\newsContent\\{}.txt".format(number),'w')
+                file.write(content)
+                file.close()
+            except:
+                #rspCode 403:內文更新失敗
+                return jsonify({"rspCode":"403"})
+        return jsonify({"rspCode":"200"})
+    else:
+        return jsonify({"rspCode":"300"})
+        
+#刪除最新消息
+#在網址帶上要刪除的編號
+#回傳rspCode
+@test.route("/delete_news/<number>", methods = ['POST'])
+def delete_news(number):
+    if request.method == 'POST':
+        #刪除資料庫中資料
+        try:
+            db.engine.execute(delete_news(number))
+        except: 
+            #rspCode 400:資料庫資料刪除失敗
+            return jsonify({"rspCode" : "400"})
+        #刪除伺服器中檔案(路徑未定)
+        if os.path.isfile(app.config["UPLOAD_FOLDER"] + '\\uploadFile\\newsImage' + "\\{}.jpg".format(number)):
+            newsImage = app.config["UPLOAD_FOLDER"] + '\\uploadFile\\newsImage' + "\\{}.jpg".format(number)
+        else:
+            #rspCode 401:圖片檔案不存在
+            return jsonify({"rspCode" : "401"})
+        newsContent = os.getcwd() + "\\newsContent\\{}.txt".format(number)
+        try:
+            os.remove(newsImage)
+        except:
+            #rspCode 402:圖片刪除失敗
+            return jsonify({"rspCode" : "402"})
+        try:
+            os.remove(newsContent)
+            return jsonify({"rspCode" : "200"})
+        except:
+            #rspCode 403:內文刪除失敗
+            return jsonify({"rspCode" : "403"})
+    else:
+        return jsonify({"rspCode":"300"})

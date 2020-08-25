2a3ce2c0f78cd38b24d1b83d961aa1e7ea135fc0
swim135792468o <swim135792468o@taipay.com>
2020-08-25 06:40:54 +0000

    fix apply_judge

--- app/controllers/apply.py
+++ app/controllers/apply.py
@@ -641,7 +641,7 @@ def apply_judge():
             plus = userPoint + int(quota)
             if period != 0:
                 rest = db.engine.execute(show_rest_time_by_applyID(applyID)).fetchone()[0]
-                db.engine.execute(alter_apply_rest_time(applyID,rest))
+                db.engine.execute(alter_apply_rest_time(applyID,rest - period))
             u = db.session.query(account).filter(account.userID == userID).first()
             u.userPoint = plus
             db.session.commit()
@@ -697,7 +697,7 @@ def apply_judge():
             db.engine.execute(plus_user_point(plus,userID))
             if period != 0:
                 rest = db.engine.execute(show_rest_time_by_applyID(applyID)).fetchone()[0]
-                db.engine.execute(alter_apply_rest_time(applyID,rest))
+                db.engine.execute(alter_apply_rest_time(applyID,rest - period))
             for x in range(int(quotaChange)):
                 pointID = make_point()+"_{}".format(str(db.session.query(point).count() + 1))
                 db.engine.execute(make_point_sql(pointID,adminID,userID))
